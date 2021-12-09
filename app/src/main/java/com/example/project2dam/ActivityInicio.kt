85e524404265082d82efbd7ad5b3342568aa6525
norbertovargas <norbertovargasislaverde@gmail.com>
2021-12-09 19:43:22 +0100

    Creacion actividad inicio + layout

---
+++ app/src/main/java/com/example/project2dam/ActivityInicio.kt
@@ -0,0 +1,11 @@
+package com.example.project2dam
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ActivityInicio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
