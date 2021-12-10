06961669b5082aaba2b7db86bc03c65ee66585de
pmolina4 <pablomolinaconde4@gmail.com>
2021-12-10 20:30:02 +0100

    Firebase login 1.0

--- app/src/main/java/com/example/project2dam/ActivityLogin.kt
+++ app/src/main/java/com/example/project2dam/ActivityLogin.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
 
 class ActivityLogin : AppCompatActivity() {
@@ -26,10 +28,27 @@ class ActivityLogin : AppCompatActivity() {
     }
 
     public fun login() {
-        var pass = txtUsu?.findViewById<EditText>(R.id.txtContrasena)?.text
-        var usu = txtUsu?.findViewById<EditText>(R.id.txtLogin)?.text
+        var pass = txtUsu?.findViewById<EditText>(R.id.txtContrasena)?.text.toString()
+        var usu = txtUsu?.findViewById<EditText>(R.id.txtLogin)?.text.toString()
 
-        FirebaseAuth.getInstance().createUserWithEmailAndPassword()
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(pass,usu).addOnCompleteListener{
+            if(it.isSuccessful){
+                Toast.makeText(this,"jeee",Toast.LENGTH_SHORT)
+            } else {
+                showAlert()
+            }
+        }
+
+    }
+
+    public fun showAlert(){
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Fallo Iniciando Sesión")
+        builder.setMessage("Usuario o Contraseña Incorrectos!")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog:AlertDialog = builder.create()
+        dialog.show()
 
     }
 
