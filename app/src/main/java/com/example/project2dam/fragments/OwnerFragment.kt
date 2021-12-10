2e6c3c3d01819c5e189fcadc1f78f8bde2ee69ef
norbertovargas <norbertovargasislaverde@gmail.com>
2021-12-10 17:22:58 +0100

    Creacion fragments para registro

---
+++ app/src/main/java/com/example/project2dam/fragments/OwnerFragment.kt
@@ -0,0 +1,21 @@
+package com.example.project2dam.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.project2dam.R
+
+class OwnerFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_owner, container, false)
+    }
+
+}
