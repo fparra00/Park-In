77134c70f9e763603fe2fc1f23001e3d7467f8f3
pmolina4 <pablomolinaconde4@gmail.com>
2021-12-10 19:55:44 +0100

    ee

--- app/src/main/java/com/example/project2dam/ActivityLogin.kt
+++ app/src/main/java/com/example/project2dam/ActivityLogin.kt
@@ -6,9 +6,12 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
 
 class ActivityLogin : AppCompatActivity() {
 
+    public var txtUsu: EditText? = null
+    public var txtPass: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +20,17 @@ class ActivityLogin : AppCompatActivity() {
 
     }
 
-    fun pantallaInicio(view: View?) {
+    public fun pantallaInicio(view: View?) {
         val cambiarPantalla = Intent(this, ActivityInicio::class.java)
         startActivity(cambiarPantalla)
     }
 
+    public fun login() {
+        var pass = txtUsu?.findViewById<EditText>(R.id.txtContrasena)?.text
+        var usu = txtUsu?.findViewById<EditText>(R.id.txtLogin)?.text
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword()
+
+    }
 
 }
