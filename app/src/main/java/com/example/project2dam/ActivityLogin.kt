e7c95b0d9b8112a3e0b87df2633ed96c9dbd7d8e
norbertovargas <norbertovargasislaverde@gmail.com>
2021-12-12 18:16:42 +0100

    Revert "."
    This reverts commit 41ca61e611554f9236459edf1df311b6d1f03ad5.

--- app/src/main/java/com/example/project2dam/ActivityLogin.kt
+++ app/src/main/java/com/example/project2dam/ActivityLogin.kt
@@ -6,9 +6,14 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
 
 class ActivityLogin : AppCompatActivity() {
 
+    public var txtUsu: EditText? = null
+    public var txtPass: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,10 +21,35 @@ class ActivityLogin : AppCompatActivity() {
 
 
     }
-    fun pantallaInicio(view: View?) {
+
+    public fun pantallaInicio(view: View?) {
         val cambiarPantalla = Intent(this, ActivityInicio::class.java)
         startActivity(cambiarPantalla)
     }
 
+    public fun login() {
+        var pass = txtUsu?.findViewById<EditText>(R.id.txtContrasena)?.text.toString()
+        var usu = txtUsu?.findViewById<EditText>(R.id.txtLogin)?.text.toString()
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(pass,usu).addOnCompleteListener{
+            if(it.isSuccessful){
+                Toast.makeText(this,"jeee",Toast.LENGTH_SHORT)
+            } else {
+                showAlert()
+            }
+        }
+
+    }
+
+    public fun showAlert(){
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Fallo Iniciando Sesión")
+        builder.setMessage("Usuario o Contraseña Incorrectos!")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog:AlertDialog = builder.create()
+        dialog.show()
+
+    }
 
 }
