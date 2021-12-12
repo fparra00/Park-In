1cc747e13566c098301bf4e324a626cedce022ed
Fernando Parra <57759900+fparra00@users.noreply.github.com>
2021-12-13 00:50:18 +0100

    Login

--- app/src/main/java/com/example/project2dam/ActivityLogin.kt
+++ app/src/main/java/com/example/project2dam/ActivityLogin.kt
@@ -2,8 +2,9 @@ package com.example.project2dam
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class ActivityLogin : AppCompatActivity() {
@@ -16,10 +17,6 @@ class ActivityLogin : AppCompatActivity() {
         setup()
     }
 
-    fun pantallaInicio(view: View?) {
-        val cambiarPantalla = Intent(this, ActivityInicioClient::class.java)
-        startActivity(cambiarPantalla)
-    }
 
     /*
     Funcion para recuperar el bundle, en caso de que, el usuario se registre de manera exitosa, se mostrara su correo y solo tendrá que introducir
@@ -31,7 +28,48 @@ class ActivityLogin : AppCompatActivity() {
         if (nom != null) {
             txtLogin.setText(nom)
         }
+
+        //Funcion on click del boton de login
+        btnIniciaSesion.setOnClickListener {
+            //Comprobacion de Login
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                txtLogin.text.toString(),
+                txtContrasena.text.toString()
+            ).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    pantallaInicio()
+                } else {
+                    showAlert(R.string.errorInicio)
+                    clearForm()
+                }
+            }
+        }
     }
 
+
+    /*
+    Funcion para lanzar alert en caso de fallo registrando al usuario
+    */
+    private fun showAlert(msj: Int) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Login Fallido")
+        builder.setMessage(msj)
+        builder.setPositiveButton("Reintentar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+
+    //Funcion para limpiar el formulario
+    private fun clearForm() {
+        txtLogin.setText(" ")
+        txtContrasena.setText(" ")
+    }
+
+    //Funcion para cambiar a la pantalla de inicio
+    fun pantallaInicio() {
+        val cambiarPantalla = Intent(this, ActivityInicioClient::class.java)
+        startActivity(cambiarPantalla)
+    }
 
 }
