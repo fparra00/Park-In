44691f3d531edf492c6a3ecc4547abbcdec5887f
Fernando Parra <57759900+fparra00@users.noreply.github.com>
2021-12-13 20:07:56 +0100

    Color Picker

--- app/src/main/java/com/example/project2dam/ActivityAddCar.kt
+++ app/src/main/java/com/example/project2dam/ActivityAddCar.kt
@@ -1,19 +1,50 @@
 package com.example.project2dam
 
-import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.project2dam.fragments.CarFragment
+import dev.sasikanth.colorsheet.ColorSheet
+import kotlinx.android.synthetic.main.register_car.*
 
-class ActivityAddCar: AppCompatActivity(){
+class ActivityAddCar : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.register_car)
-
+    //Aux Picker Color
+    companion object {
+        private const val COLOR_SELECTED = "selectedColor"
+        private const val NO_COLOR_OPTION = "noColorOption"
     }
 
 
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        var selectedColor: Int = ColorSheet.NO_COLOR
+        var noColorOption = false
+        val colors = resources.getIntArray(R.array.colors)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register_car)
+
+        //Funcion para Picker de Color
+        selectedColor = savedInstanceState?.getInt(COLOR_SELECTED) ?: colors.first()
+        noColorOption = savedInstanceState?.getBoolean(NO_COLOR_OPTION) ?: false
+
+        //onClick de picker de color
+        btnColor.setOnClickListener {
+            ColorSheet().cornerRadius(8)
+                .colorPicker(
+                    colors = colors,
+                    noColorOption = noColorOption,
+                    selectedColor = selectedColor,
+                    listener = { color ->
+                        selectedColor = color
+                        btnColor.setBackgroundColor(selectedColor)
+                    })
+                .show(supportFragmentManager)
+        }
+    }
+
 
 }
+
+
+
