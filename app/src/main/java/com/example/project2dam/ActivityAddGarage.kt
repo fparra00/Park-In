a0d982cb65770477dd65329f204c38ea486c88c8
Fernando Parra <57759900+fparra00@users.noreply.github.com>
2021-12-13 23:53:40 +0100

    Create Garages
    Co-Authored-By: norbertovargas <60181564+norbertovargas@users.noreply.github.com>
    Co-Authored-By: pmolina4 <74369952+pmolina4@users.noreply.github.com>

--- app/src/main/java/com/example/project2dam/ActivityAddGarage.kt
+++ app/src/main/java/com/example/project2dam/ActivityAddGarage.kt
@@ -1,25 +1,54 @@
 package com.example.project2dam
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_add_garage.*
 import kotlinx.android.synthetic.main.register_car.*
 
 class ActivityAddGarage : AppCompatActivity() {
+
+    //Var Aux
+    private val db = FirebaseFirestore.getInstance()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_garage)
 
         btnRegisterGarage.setOnClickListener {
 
+            //Creamos un garaje y lo almacenamos en la bdd
+            createGaraje()
+            //Lo mandamos por Bundle
             val bundle = Bundle()
             val intent = Intent(this, ActivityInicioOwner::class.java).apply {
                 bundle.putInt("garage", 1)
             }
             intent.putExtras(bundle)
             startActivity(intent)
-
         }
+    }
+
+    /*
+     Funcion para crear un coche e introducirlo en BDD
+   */
+    public fun createGaraje() {
+        val user = Firebase.auth.currentUser?.email
+
+        val messageRef = db
+            .collection("users").document(user.toString())
+            .collection("garages").document(txtDireccion.text.toString())
+
+        messageRef.set(
+            hashMapOf(
+                "Address" to txtDireccion.text.toString(),
+                "Number of Slots" to txtNumeroPlazas.text.toString(),
+                "City" to txtCiudadGarage.text.toString(),
+            )
+        )
     }
 }
