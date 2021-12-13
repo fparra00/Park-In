277ceafe35a5fa3d24c6027b26ee910e520fb557
pmolina4 <pablomolinaconde4@gmail.com>
2021-12-13 17:11:49 +0100

    Integración recycler garaje
    continuado con la intgracion del recyccler de garaje

--- app/src/main/java/com/example/project2dam/recycler/GarageViewHolder.kt
+++ app/src/main/java/com/example/project2dam/recycler/GarageViewHolder.kt
@@ -1,11 +1,15 @@
 package com.example.project2dam.recycler
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.project2dam.R
 
 class GarageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val garage: TextView by lazy { itemView.findViewById(R.id.txtGarage) }
+    val direccionGarage:TextView by lazy { itemView.findViewById(R.id.txtDireccionGarage) }
+    val ciudadGarage:TextView by lazy { itemView.findViewById(R.id.txtCiudad) }
+    val numeroPlazas:TextView by lazy { itemView.findViewById(R.id.txtNplazas) }
+    val imgBorrarGarage: ImageView by lazy { itemView.findViewById(R.id.ic_removeGarage) }
 
 }
